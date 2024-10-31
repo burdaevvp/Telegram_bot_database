413fe021fb7249d10d57c44581666a1eee2e19a1
unknown <volodymyr.burdaev@khpi.edu.ua>
2024-10-31 16:03:27 +0200

    data_shop

--- Database/MysqlConnect.php
+++ Database/MysqlConnect.php
@@ -10,15 +10,11 @@ class MysqlConnect implements Connectable {
     public function __construct(){
         $config = Config::getDatabase();
 
-//        $this->connect = mysqli_connect($config["host"], $config["username"],
-//            $config["password"], $config["database"], $config["port"]);
 
         $this->connect = mysqli_connect($config["host"],
             $config["username"], $config["password"], $config["database"]);
 
-  //      echo $config["host"];echo "<br>"; echo $config["username"];echo "<br>";
-  //      echo $config["password"]; echo "<br>"; echo $config["database"];
-   //     $conn = new mysqli($servername, $username, $password, $dbname);
+
 
         if(mysqli_connect_error()){
             throw new Exception("Failed mysql connect.");
